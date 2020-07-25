80bdcd09709cec6ffc15f5d9c2b61a29f2b5ede8
Édson Martins <edson.martins@garupa.co>
2020-07-25 09:21:46 -0300

    habilitando autenticacao

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import UserController from './app/controllers/UserController';
 import AuthController from './app/controllers/AuthController';
 import CardController from './app/controllers/CardController';
 
-// import authMiddleware from './app/middlewares/auth';
+import authMiddleware from './app/middlewares/auth';
 import checkCard from './app/middlewares/checkCard';
 
 const routes = Router();
@@ -16,7 +16,7 @@ routes.get('/', (req, res) => res.json('API-SCRAPBOOK-STRUTURE'));
 routes.post('/users', UserController.store);
 routes.post('/login', AuthController.store);
 
-// routes.use(authMiddleware);
+routes.use(authMiddleware);
 
 routes.get('/cards', CardController.index);
 routes.get('/cards/:id', checkCard, CardController.show);
